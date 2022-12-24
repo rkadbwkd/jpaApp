4fdb696fa5462b3a47351b67994b47b7b19e8872
jihwan <rkadbwkd@nate.com>
2022-12-24 22:18:17 +0900

    주문 도메인 개발 - 주문 검색 기능 ㄱ애발

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,8 +21,20 @@ public class OrderRepository {
         return em.find(Order.class,id);
         
     }
-    // 동적쿼리 추후 수정예쩡
-    // public List<Order> findAll(OrderSearch orderSearch){}
+    // 동적쿼리 추후 수정예쩡(query dsl로)
+     public List<Order> findAll(OrderSearch orderSearch){
+
+
+         List<Order> resultList = em.createQuery("select o from Order o join o.member m" +
+                 " where o.status = :status " +
+                 " and m.name like :name", Order.class)
+                 .setParameter("status", orderSearch.getOrderStatus())
+                 .setParameter("name", orderSearch.getMemberName())
+                 .setMaxResults(1000)
+                 .getResultList();
+
+         return resultList;
+     }
     
     
 }
