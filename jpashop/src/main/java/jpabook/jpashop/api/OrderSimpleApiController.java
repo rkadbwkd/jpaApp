3409d76323eeadee4b3b02e84a1acbeb94d27f0a
jihwan <rkadbwkd@nate.com>
2022-12-31 16:28:03 +0900

    JPA2 - API 개발 고급 - 지연 ㄹ로딩과 조회 성능 최적화

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.api;
+
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery ( Order가 Member와 Delivery만 연관관계를 걸리게함)
+ * */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress();
+
+        }
+        return all; // Order > Member , Member > Order 무한로프에 빠짐
+    }
+
+
+
+
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,9 +23,10 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
-
+    @JsonIgnore
     private Order order;
 
     private int orderPrice; // 주문가격
